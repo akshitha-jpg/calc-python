03bb4849b558a9f9246fd16affbd1e7613902223
akshitha-jpg <akshithamallavarapu.1@gmail.com>
2025-11-24 14:55:27 +0530

    Update test_main.py

--- src/test_main.py
+++ src/test_main.py
@@ -1,28 +1,20 @@
 import unittest
-from app import app  # import your Flask app
+from app import app
 
 class TestFlaskApp(unittest.TestCase):
 
     def setUp(self):
-        # Create a test client
-        self.app = app.test_client()
-        self.app.testing = True
+        self.client = app.test_client()
+        self.client.testing = True
 
     def test_home_status_code(self):
-        # Test that the home page returns HTTP 200
-        response = self.app.get("/")
+        response = self.client.get("/")
         self.assertEqual(response.status_code, 200)
 
     def test_home_content(self):
-        # Test that the home page contains some expected text
-        response = self.app.get("/")
+        response = self.client.get("/")
         self.assertIn(b"Simple Movie Site", response.data)
-        self.assertIn(b"Top Movies", response.data)
-        self.assertIn(b"The Shawshank Redemption", response.data)
         self.assertIn(b"Inception", response.data)
-        self.assertIn(b"Interstellar", response.data)
-        self.assertIn(b"The Dark Knight", response.data)
-        self.assertIn(b"Avengers: Endgame", response.data)
 
 if __name__ == "__main__":
     unittest.main()
