182bab7abfb1b3df3123c9e201b231e9bc41f33b
akshitha-jpg <akshithamallavarapu.1@gmail.com>
2025-11-24 10:20:43 +0530

    Create main.py

---
+++ src/main.py
@@ -0,0 +1,22 @@
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route("/")
+def home():
+    return """
+    <h1>🎬 Simple Movie Site</h1>
+    <p>Welcome to my movie list!</p>
+
+    <h2>Top Movies</h2>
+    <ul>
+        <li>The Shawshank Redemption</li>
+        <li>Inception</li>
+        <li>Interstellar</li>
+        <li>The Dark Knight</li>
+        <li>Avengers: Endgame</li>
+    </ul>
+    """
+
+if __name__ == "__main__":
+    app.run(debug=True)
